678e77f6376257581a7ca0053901e1a7a774e42c
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2017-08-02 00:22:49 -0700

    Initial mongodb setup

--- receipii/app.js
+++ receipii/app.js
@@ -4,6 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//Mongodb
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/receipii');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Make Database Accessible
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
