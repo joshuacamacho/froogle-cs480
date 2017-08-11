b57a342014064ffb003414445459b1b1706cece7
Joshua Camacho <joshuajcamacho@gmail.com>
2017-08-10 20:19:04 -0700

    combining all changes, fixing styles

--- receipii/app.js
+++ receipii/app.js
@@ -16,6 +16,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
