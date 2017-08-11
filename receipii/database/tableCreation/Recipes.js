20a86ddd177fea7ab86106a3424438bf11af9049
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2017-08-11 16:00:38 -0700

    Table initializer function added

--- receipii/database/tableCreation/Recipes.js
+++ receipii/database/tableCreation/Recipes.js
@@ -20,3 +20,10 @@ var params = {
 		WriteCapacityUnits: 5		
 	}
 }
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
